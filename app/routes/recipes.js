fd2843e8841de527f1f4863db519bf22a0b319cf
Damien White <damien.white@visoftinc.com>
2020-01-22 10:40:22 -0500

    :construction: The model hook with static data

--- app/routes/recipes.js
+++ app/routes/recipes.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class RecipesRoute extends Route {
+  model() {
+    return [
+      { name: 'Yummy Burger' },
+      { name: 'Footlong Hot Dog' },
+      { name: 'Crispy Chicken Sandwich' },
+    ]
+  }
 }
